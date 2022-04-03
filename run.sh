c5a0ead6b2ab1aa1d3a906fb1eb6272677f5e068
YuZhang10 <zhangyuTHU14@gmail.com>
2022-04-03 22:14:09 +0800

    fixbugs

--- run.sh
+++ run.sh
@@ -5,15 +5,14 @@
 # about how to use PyTorch's distributed data parallel.
 pretrain="cyclone/simcse-chinese-roberta-wwm-ext"
 date='0403'
-epoch=4
-bs=128
+epoch=2
+bs=64
 pooler="cls"
 max_seq_length=64
 
 dir_path="./result/${date};ep${epoch};bs${bs};${pooler};max_seq_length${max_seq_length};"
 drive_result="/content/drive/MyDrive/competition/simcse-mini/result"
-# 保留原始脚本
-cp ./run.sh $dir_path
+
 # 训练模型
 python 2.train.py \
     --model_name_or_path $pretrain \
@@ -25,20 +24,23 @@ python 2.train.py \
     --per_device_eval_batch_size $bs \
     --learning_rate 3e-5 \
     --max_seq_length $max_seq_length \
-    --save_total_limit 2 \
+    --save_total_limit 1 \
     --evaluation_strategy steps \
     --greater_is_better False \
     --metric_for_best_model eval_loss \
     --load_best_model_at_end \
-    --eval_steps 100 \
-    --save_steps 100 \
-    --logging_steps 50 \
+    --eval_steps 5 \
+    --save_steps 5 \
+    --logging_steps 5 \
     --pooler_type $pooler \
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
     --do_eval
 echo "train finished!"
+
+# 保留原始脚本
+cp ./run.sh $dir_path/run_backup.sh
 
 # 提取embedding
 python 3.get_embedding.py --dir_path $dir_path
