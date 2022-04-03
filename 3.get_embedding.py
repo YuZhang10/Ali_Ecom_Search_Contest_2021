116e75aefa4d455c0d914f2eaf91dd278face9d4
YuZhang10 <zhangyuTHU14@gmail.com>
2022-04-03 13:27:40 +0800

    fixbug:修复一些bug

--- 3.get_embedding.py
+++ 3.get_embedding.py
@@ -12,32 +12,31 @@ from simcse.models import BertForCL
 from transformers import AutoTokenizer
 
 device = "cuda:0"
-tokenizer = AutoTokenizer.from_pretrained("hfl/chinese-roberta-wwm-ext")
 batch_size = 100
 use_pinyin = False
-
-
-def encode_fun(texts, model):
-    inputs = tokenizer.batch_encode_plus(texts, padding=True, truncation=True, return_tensors="pt", max_length=115)
-    inputs.to(device)
-    with torch.no_grad():
-        embeddings = model(**inputs, output_hidden_states=True, return_dict=True, sent_emb=True).pooler_output
-        embeddings = embeddings.squeeze(0).cpu().numpy()
-    return embeddings
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--dir_path', type=str, default="./result/sup-simcse/")
+    # parser.add_argument('--pretrain', type=str, default="hfl/chinese-roberta-wwm-ext")
     args = parser.parse_args()
     print(args)
     model = BertForCL.from_pretrained(args.dir_path)
     model.to(device)
     corpus = [line[1] for line in csv.reader(open("./data/corpus.tsv"), delimiter='\t')]
     query = [line[1] for line in csv.reader(open("./data/dev.query.txt"), delimiter='\t')]
+    tokenizer = AutoTokenizer.from_pretrained(args.dir_path)
 
+    def encode_fun(texts, model):
+        inputs = tokenizer.batch_encode_plus(texts, padding=True, truncation=True, return_tensors="pt", max_length=115)
+        inputs.to(device)
+        with torch.no_grad():
+            embeddings = model(**inputs, output_hidden_states=True, return_dict=True, sent_emb=True).pooler_output
+            embeddings = embeddings.squeeze(0).cpu().numpy()
+        return embeddings
+
+    print("Processing query embedding...")
     query_embedding_file = csv.writer(open(os.path.join(args.dir_path,'query_embedding'), 'w'), delimiter='\t')
-
     for i in tqdm(range(0, len(query), batch_size)):
         batch_text = query[i:i + batch_size]
         temp_embedding = encode_fun(batch_text, model)
@@ -47,6 +46,7 @@ if __name__ == '__main__':
             writer_str = ','.join(writer_str)
             query_embedding_file.writerow([i + j + 200001, writer_str])
 
+    print("Processing doc embedding...")
     doc_embedding_file = csv.writer(open(os.path.join(args.dir_path,'doc_embedding'), 'w'), delimiter='\t')
     for i in tqdm(range(0, len(corpus), batch_size)):
         batch_text = corpus[i:i + batch_size]
--- run.sh
+++ run.sh
@@ -3,16 +3,15 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
-DIR_PATH="./result/0402;simcse-chinese-roberta;bs128;nomlp;cls"
+DIR_PATH="./result/0402;simcse-chinese-roberta;ep4;bs128;nomlp;cls"
 DRIVE_RESULT="/content/drive/MyDrive/competition/simcse-mini/result"
-
 # 训练模型
 python train.py \
-    --model_name_or_path cyclone/simcse-chinese-roberta-wwm-ext \
+    --model_name_or_path $PRETRAIN \
     --train_file "./data/X_train.csv" \
     --validation_file "./data/X_val.csv" \
     --output_dir $DIR_PATH \
-    --num_train_epochs 2 \
+    --num_train_epochs 4 \
     --per_device_train_batch_size 128 \
     --per_device_eval_batch_size 128 \
     --learning_rate 3e-5 \
@@ -34,8 +33,9 @@ echo "train finished!"
 python 3.get_embedding.py --dir_path $DIR_PATH
 
 # 打包embedding，放入result文件夹
-bash 4.tar.sh
-mv foo.tar.gz $DIR_PATH/foo.tar.gz
+tar zcvf $DIR_PATH/foo.tar.gz  \
+$DIR_PATH/query_embedding \
+$DIR_PATH/doc_embedding
 
 # 将文件移回云盘保存
 cp -r ./result/* $DRIVE_RESULT
